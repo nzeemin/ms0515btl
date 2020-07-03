6ec9aaf4c676b4e2b10703277be66d6eb24b0cfe
Nikita Zimin <nzeemin@gmail.com>
2020-07-04 00:38:59 +0300

    Emulator project files moved into sub-folder.

--- emulator/!astyle.bat
+++ emulator/!astyle.bat
@@ -1,4 +1,4 @@
 @echo off
 set ASTYLEEXE=c:\bin\astyle.exe
-set ASTYLEOPT=-n -Q --options=astyle-cpp-options
+set ASTYLEOPT=-n -Q --options=..\astyle-cpp-options
 %ASTYLEEXE% %ASTYLEOPT% emubase\*.h emubase\*.cpp Util\*.h Util\*.cpp *.cpp *.h
